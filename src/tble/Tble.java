3c87925c19f4f7a65e1a9dca53521110a1eda13d
Patrick <patmatayabas@gmail.com>
2022-01-31 23:57:39 +0800

    First commit

---
+++ src/tble/Tble.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tble;
+
+/**
+ *
+ * @author patma
+ */
+public class Tble {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        NewJFrame x = new NewJFrame();
+        x.setVisible(true);
+    }
+    
+    
+}
